50b811ebabcfdfb467db1a14272715c7e3e936ce
CQBerry <chinq6624@gmail.com>
2022-09-11 13:32:48 +0800

    chore: adjust animation

--- src/components/TodoList/TodoList.vue
+++ src/components/TodoList/TodoList.vue
@@ -2,16 +2,17 @@
   <!-- add / delete / pin 动画 -->
   <TransitionGroup id="todo-list" name="list" tag="div">
     <!--  如果 hideCheckedTodos 为 true，则遍历 todoChecked === false 的 Todo 的 todos 数组  -->
-    <Todo v-for="todo of (todoStore.hideCheckedTodos ? todoStore.uncheckedTodos : todoStore.todos)"
-          :todo-id="todo.todoId"
-          :key="todo.todoId"
-    ></Todo>
+    <TodoItem
+        v-for="todo of (todoStore.hideCheckedTodos ? todoStore.uncheckedTodos : todoStore.todos)"
+        :todo-id="todo.todoId"
+        :key="todo.todoId"
+    ></TodoItem>
     <AddTodo key="0"></AddTodo>
   </TransitionGroup>
 </template>
 
 <script lang="ts" setup>
-import Todo from './Todo.vue';
+import TodoItem from './TodoItem.vue';
 import AddTodo from './AddTodo.vue'
 import { useStore } from '@/stores/todoStore'
 
@@ -34,7 +35,7 @@ const todoStore = useStore()
 .list-enter-from,
 .list-leave-to {
   opacity: 0;
-  transform: translate3d(-30px, -30px, 0px);
+  transform: translate3d(-50%, -50%, 0px);
 }
 
 .list-leave-active {
