06888368f72084a5b156d3308f474b377f78b3d7
CQBerry <chinq6624@gmail.com>
2022-09-19 08:39:56 +0800

    chore: edit style in AddTodoInputArea.vue

--- src/components/TodoList/AddTodoInputArea.vue
+++ src/components/TodoList/AddTodoInputArea.vue
@@ -1,23 +1,21 @@
 <template>
-  <h1>
-    <input
-        v-model.lazy="todoStore.addedTodo.todoTitle"
-        class="rounded-md
-               bg-todoLight
-               dark:bg-todoGray
-               border-black
-               dark:border-stone-100
-               border-dotted
-               border-2
-               dark:placeholder-blue-200
-               dark:text-stone-100
-               col-span-2
-               pl-0.5"
-        placeholder="Todo title (required)"
-        required
-        type="text"
-    >
-  </h1>
+  <input
+      v-model.lazy="todoStore.addedTodo.todoTitle"
+      class="rounded-md
+             bg-todoLight
+             dark:bg-todoGray
+             border-black
+             dark:border-stone-100
+             border-dotted
+             border-2
+             dark:placeholder-blue-200
+             dark:text-stone-100
+             col-span-2
+             pl-1"
+      placeholder="Todo title (required)"
+      required
+      type="text"
+  >
   <textarea
       v-model.lazy="todoStore.addedTodo.todoDescription"
       class="rounded-md
@@ -31,7 +29,7 @@
              dark:text-stone-100
              resize-none
              col-span-3
-             pl-0.5"
+             pl-1"
       placeholder="Todo description">
   </textarea>
 </template>
