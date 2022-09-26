e223392e9c24be2bb3f2f3feb3f39dcdb56fa1ff
CQBerry <chinq6624@gmail.com>
2022-09-26 15:15:22 +0800

    feat: supports dark and light favicons, toggles with color mode

--- src/components/FunctionsBar/ToggleColorMode.vue
+++ src/components/FunctionsBar/ToggleColorMode.vue
@@ -1,13 +1,17 @@
 <template>
-  <a id="toggle-color-mode" class="font-mono actions function-colors" @click="toggleColorMode()">{{ emoji }} Toggle color mode</a>
+  <a id="toggle-color-mode" class="font-mono actions function-colors" @click="toggleColorMode()">{{ emoji }} Toggle
+    color mode</a>
 </template>
 
 <script lang="ts" setup>
-import { useDark, useToggle } from '@vueuse/core'
+import { useDark, useToggle, useFavicon } from '@vueuse/core'
 import { computed } from 'vue'
 
 const isDark = useDark()
 const toggleColorMode = useToggle(isDark)
+
+const favicon = computed(() => isDark.value ? 'dark.png' : 'light.png')
+useFavicon(favicon)
 
 const emoji = computed(() => isDark.value ? '🤍' : '🖤')
 </script>
