42d5657c0a49d8a7a6a1670f66d16e45f573b3c1
CQBerry <chinq6624@gmail.com>
2022-09-27 18:15:40 +0800

    refactor(auto save): I forgot something called "watch"

--- src/components/TodoList/TodoList.vue
+++ src/components/TodoList/TodoList.vue
@@ -1,33 +1,49 @@
 <template>
-  <TransitionGroup id="todo-list" class="grid gap-5 grid-cols-todo auto-rows-fr" name="list" tag="div">
-    <TodoItem v-for="todo of (todoStore.hideCheckedTodos ? todoStore.uncheckedTodos : todoStore.todos)"
-      :key="todo.todoId" :todo-id="todo.todoId"></TodoItem>
-    <AddTodo key="0"></AddTodo>
-  </TransitionGroup>
+    <TransitionGroup id="todo-list" class="grid gap-5 grid-cols-todo auto-rows-fr" name="list" tag="div">
+        <TodoItem v-for="todo of (todoStore.hideCheckedTodos ? todoStore.uncheckedTodos : todoStore.todos)"
+            :key="todo.todoId" :todo-id="todo.todoId"></TodoItem>
+        <AddTodo key="0"></AddTodo>
+    </TransitionGroup>
 </template>
 
 <script lang="ts" setup>
 import TodoItem from './TodoItem.vue'
 import AddTodo from './AddTodo.vue'
 import { useStore } from '@/stores/todoStore'
+import { useIdle } from '@vueuse/core'
+import { watch } from 'vue'
 
 const todoStore = useStore()
+
+// 离开页面时保存
+window.addEventListener('beforeunload', () => {
+    todoStore.updateTodo()
+})
+
+const { idle } = useIdle(5000)
+watch(idle, () => {
+    if (idle.value) {
+        todoStore.updateTodo()
+        console.log('updated')
+    }
+})
+
 </script>
 
 <style scoped>
 .list-move,
 .list-enter-active,
 .list-leave-active {
-  transition: all 0.5s ease;
+    transition: all 0.5s ease;
 }
 
 .list-enter-from,
 .list-leave-to {
-  opacity: 0;
-  transform: translate3d(-50%, -50%, 0px);
+    opacity: 0;
+    transform: translate3d(-50%, -50%, 0px);
 }
 
 .list-leave-active {
-  position: absolute;
+    position: absolute;
 }
 </style>
