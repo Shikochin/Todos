9fa7f0615737c977c60f60273ab8a8372f38f6fc
CQBerry <chinq6624@gmail.com>
2022-10-02 15:05:31 +0800

    fix: why did I add this annoying rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,10 +24,6 @@ module.exports = {
             'error',
             4
         ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
         'quotes': [
             'error',
             'single'
