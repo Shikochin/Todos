74575717bd048a9748506a70a6d71280871979a9
CQBerry <chinq6624@gmail.com>
2022-10-07 10:39:23 +0800

    chore: move the location of static assets; style => styles

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import App from './App.vue'
 import { createApp } from 'vue'
-import './style/main.scss'
+import './styles/main.scss'
 import { createPinia } from 'pinia'
 
-const app = createApp(App)
 const pinia = createPinia()
-app.use(pinia)
-app.mount('#app')
+
+createApp(App).use(pinia).mount('#app')
 
 // eslint-disable-next-line no-console
 console.log('😎 Todos! by CHINQ')
